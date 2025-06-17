667d4f9aeac8a2c51abe6470a05efdad3162a344
zt <lzttzllzttzl@gmail.com>
2025-06-17 13:23:12 +0800

    database setup

---
+++ src/firebase.js
@@ -0,0 +1,23 @@
+// src/firebase.js
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth"; // if using auth
+import { getDatabase } from "firebase/database";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBudntLto0z_wWhVwNJlzCVeK7BN1bZGjU",
+  authDomain: "greenlight-c9b3e.firebaseapp.com",
+  projectId: "greenlight-c9b3e",
+  storageBucket: "greenlight-c9b3e.firebasestorage.app",
+  messagingSenderId: "814210917189",
+  appId: "1:814210917189:web:99d9bf6f06c1da64c2942c",
+  databaseURL: "https://greenlight-c9b3e-default-rtdb.asia-southeast1.firebasedatabase.app/",
+  measurementId: "G-HVEDZXEP1C"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);           // optional
+// Initialize Realtime Database and get a reference to the service
+export const database = getDatabase(app);
+export default app;
